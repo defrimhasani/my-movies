1909553583dfbb703f766ff5f50ad193beddd77d
start.vaadin.com <no-reply@vaadin.com>
2023-05-19 19:33:28 +0000

    Generated project

---
+++ src/main/java/com/defrimhsn/movies/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package com.defrimhsn.movies.data.service;
+
+import com.defrimhsn.movies.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    User findByUsername(String username);
+}
